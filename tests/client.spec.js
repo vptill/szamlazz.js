d3de8dd715c2b871e149e06d4e746afd5aa4eaac
László Monda <laci@monda.hu>
2022-11-13 15:57:09 +0100

    Remove require('libxmljs2')

--- tests/client.spec.js
+++ tests/client.spec.js
@@ -3,7 +3,6 @@
 
 const fs = require('fs')
 const path = require('path')
-const xmljs = require('libxmljs2')
 const axios = require('axios')
 const sinon = require('sinon')
 const chai = require('chai')
