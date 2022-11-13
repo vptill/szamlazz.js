87c5af9b61a54e8631782de79790bd795ebeb0c0
László Monda <laci@monda.hu>
2022-11-13 15:52:34 +0100

    Don't reference the non-existing es5 version, and remove esnextguardian.

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 'use strict'
 
-module.exports = require('esnextguardian')(
-  require('path').join(__dirname, 'src', 'index.js'),
-  require('path').join(__dirname, 'es5', 'index.js'),
-  require
-)
+module.exports = require(require('path').join(__dirname, 'src', 'index.js'))
