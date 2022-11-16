6f707d777e0f0abb039caef753fcb940f3b3e451
László Monda <laci@monda.hu>
2022-11-16 12:35:05 +0100

    Export Currency, Language, PaymentMethod, and TaxSubject.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import {Buyer} from './lib/Buyer.js'
 import {Client} from './lib/Client.js'
-import {Currencies, Languages, PaymentMethods, TaxSubjects} from './lib/Constants.js'
+import {Currencies, Currency, Language, Languages, PaymentMethod, PaymentMethods, TaxSubject, TaxSubjects} from './lib/Constants.js'
 import {Invoice} from './lib/Invoice.js'
 import {Item} from './lib/Item.js'
 import {Seller} from './lib/Seller.js'
 
-export {Buyer, Client, Invoice, Item, Seller, Currencies, Languages, PaymentMethods, TaxSubjects}
+export {Buyer, Client, Invoice, Item, Seller, Currencies, Currency, Language, Languages, PaymentMethod, PaymentMethods, TaxSubject, TaxSubjects}
