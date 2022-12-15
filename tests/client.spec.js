3f652ddc155a1f6395ee661fb8ef295819c8e891
Róbert Kiss <erT78gb@gmail.com>
2022-12-15 11:07:49 +0100

    test: use nock instead of sinon (#42)

--- tests/client.spec.js
+++ tests/client.spec.js
@@ -1,24 +1,24 @@
 /* eslint-env mocha */
 
-import fs from 'fs'
-import path from 'path'
-import axios from 'axios'
-import sinon from 'sinon'
+import nock from 'nock'
 import chai from 'chai'
 import chaiAsPromised from 'chai-as-promised'
 const expect = chai.expect
 chai.use(chaiAsPromised)
 
 import {Buyer, Client, Invoice, Item, Seller} from '../index.js'
-import {createClient, createTokenClient, createSeller, createBuyer, createSoldItemNet, createSoldItemGross, createInvoice} from './resources/setup.js'
-
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-const __dirname = dirname(fileURLToPath(import.meta.url));
+import {
+  createClient,
+  createTokenClient,
+  createSeller,
+  createBuyer,
+  createSoldItemNet,
+  createSoldItemGross,
+  createInvoice,
+  RESPONSE_FILE_PATHS
+} from './resources/setup.js'
 
 describe('Client', () => {
-  let axiosStub
-
   let client
   let tokenClient
   let seller
@@ -28,7 +28,7 @@ describe('Client', () => {
   let invoice
 
   before(() => {
-    axiosStub = sinon.stub(axios, 'post')
+    nock.disableNetConnect()
   })
 
   beforeEach(() => {
@@ -42,11 +42,11 @@ describe('Client', () => {
   })
 
   afterEach(() => {
-    sinon.reset()
+    nock.cleanAll()
   })
 
   after(() => {
-    sinon.restore()
+    nock.enableNetConnect()
   })
 
   describe('constructor', () => {
@@ -66,42 +66,40 @@ describe('Client', () => {
   describe('issueInvoice', () => {
     describe('HTTP status', () => {
       it('should handle failed requests', async () => {
-        axiosStub.resolves(new Promise(r => r({ status: 404, statusText: 'Not found' })))
+        nock('https://www.szamlazz.hu')
+          .post('/szamla/')
+          .reply(404)
 
-        await expect(client.issueInvoice(invoice)).rejectedWith('404 Not found')
+        await expect(client.issueInvoice(invoice)).rejectedWith('Request failed with status code 404')
+        nock.isDone()
       })
     })
 
     describe('service error response', () => {
       it('should throw error', async () => {
-        axiosStub.resolves(new Promise(r => r({
-          status: 200,
-          headers: {
+        nock('https://www.szamlazz.hu')
+          .post('/szamla/')
+          .reply(200, undefined, {
             szlahu_error_code: '57',
             szlahu_error: 'Some error message from the remote service'
-          }
-        })))
+          })
 
         await expect(client.issueInvoice(invoice)).rejectedWith('Some error message from the remote service')
+        nock.isDone()
       })
     })
 
     describe('successful invoice generation without download request', () => {
-      beforeEach(done => {
-        fs.readFile(path.join(__dirname, 'resources', 'success_without_pdf.xml'), (e, data) => {
-          axiosStub.resolves(new Promise(r => r({
-            status: 200,
-            headers: {
-              szlahu_bruttovegosszeg: '6605',
-              szlahu_nettovegosszeg: '5201',
-              szlahu_szamlaszam: '2016-139'
-            },
-            data: Buffer.from(data).toString('utf8')
-          })))
+      beforeEach(() => {
+        nock('https://www.szamlazz.hu')
+          .post('/szamla/')
+          .replyWithFile(200, RESPONSE_FILE_PATHS.SUCCESS_WITHOUT_PDF, {
+            szlahu_bruttovegosszeg: '6605',
+            szlahu_nettovegosszeg: '5201',
+            szlahu_szamlaszam: '2016-139'
+          })
 
-          client.setRequestInvoiceDownload(false)
-          done()
-        })
+        client.setRequestInvoiceDownload(false)
       })
 
       it('should have result parameter', async () => {
@@ -111,7 +109,6 @@ describe('Client', () => {
           'netTotal',
           'grossTotal'
         )
-
       })
 
       it('should have `invoiceId` property', async () => {
@@ -134,21 +131,16 @@ describe('Client', () => {
     })
 
     describe('successful invoice generation with download request', () => {
-      beforeEach(done => {
-        fs.readFile(path.join(__dirname, 'resources', 'success_with_pdf.xml'), (e, data) => {
-          axiosStub.resolves(new Promise(r => r({
-            status: 200,
-            headers: {
-              szlahu_bruttovegosszeg: '6605',
-              szlahu_nettovegosszeg: '5201',
-              szlahu_szamlaszam: '2016-139'
-            },
-            data: Buffer.from(data).toString('utf8')
-          })))
-
-          client.setRequestInvoiceDownload(true)
-          done()
+      beforeEach(() => {
+       nock('https://www.szamlazz.hu')
+        .post('/szamla/')
+        .replyWithFile(200, RESPONSE_FILE_PATHS.SUCCESS_WITH_PDF, {
+          szlahu_bruttovegosszeg: '6605',
+          szlahu_nettovegosszeg: '5201',
+          szlahu_szamlaszam: '2016-139'
         })
+
+        client.setRequestInvoiceDownload(true)
       })
 
       it('should have result parameter', async () => {
@@ -186,17 +178,12 @@ describe('Client', () => {
 
   describe('getInvoiceData', () => {
     describe('unsuccessful invoice generation', () => {
-      beforeEach(done => {
-        fs.readFile(path.join(__dirname, 'resources', 'unknown_invoice_number.xml'), (e, data) => {
-          axiosStub.resolves(new Promise(r => r({
-            status: 200,
-            headers: {},
-            data: Buffer.from(data).toString('utf8')
-          })))
+      beforeEach(() => {
+        nock('https://www.szamlazz.hu')
+          .post('/szamla/')
+          .replyWithFile(200, RESPONSE_FILE_PATHS.UNKNOWN_INVOICE_NUMBER)
 
-          client.setRequestInvoiceDownload(true)
-          done()
-        })
+        client.setRequestInvoiceDownload(true)
       })
 
       it('should throw error', async () => {
