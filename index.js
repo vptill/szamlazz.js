d24984d4da0003578a4dff2d70bf8f6c79c6afd2
Till <hanzojka@gmail.com>
2025-01-25 12:24:06 +0100

    default export

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-import {Buyer} from './lib/Buyer.js'
-import {Client} from './lib/Client.js'
-import {Currencies, Currency, Language, Languages, PaymentMethod, PaymentMethods, TaxSubject, TaxSubjects} from './lib/Constants.js'
-import {Invoice} from './lib/Invoice.js'
-import {Item} from './lib/Item.js'
-import {Seller} from './lib/Seller.js'
+import { Buyer } from "./lib/Buyer.js";
+import { Client } from "./lib/Client.js";
+import {
+  Currencies,
+  Currency,
+  Language,
+  Languages,
+  PaymentMethod,
+  PaymentMethods,
+  TaxSubject,
+  TaxSubjects,
+} from "./lib/Constants.js";
+import { Invoice } from "./lib/Invoice.js";
+import { Item } from "./lib/Item.js";
+import { Seller } from "./lib/Seller.js";
 
-export {Buyer, Client, Invoice, Item, Seller, Currencies, Currency, Language, Languages, PaymentMethod, PaymentMethods, TaxSubject, TaxSubjects}
+export {
+  Buyer,
+  Client,
+  Invoice,
+  Item,
+  Seller,
+  Currencies,
+  Currency,
+  Language,
+  Languages,
+  PaymentMethod,
+  PaymentMethods,
+  TaxSubject,
+  TaxSubjects,
+};
+export default Client;
